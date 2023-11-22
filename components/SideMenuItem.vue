685607cc2026d26a76ef4d2bdebf86f7fabb7c0b
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-11-22 22:47:49 +0900

    Adjusting the side menu

--- components/SideMenuItem.vue
+++ components/SideMenuItem.vue
@@ -1,24 +1,48 @@
 <script setup lang="ts">
 import { TrashIcon } from "@heroicons/vue/24/outline";
 
-interface Props {
+const store = useList();
+const { selectedItemId } = storeToRefs(store);
+const { removeItem } = store;
+
+const props = defineProps<{
   id: string;
   title: string;
   selected: boolean;
-}
+}>();
 
-const props = defineProps<Props>();
+const isHover = ref(false);
+
+const classObject = computed(() => [
+  "flex",
+  "hover:opacity-80",
+  !props.selected && "opacity-100",
+  props.selected && "opacity-50 pointer-events-none",
+]);
+
+function deleteItem(id: string) {
+  if (confirm("記事を削除してもよろしいでしょうか？")) {
+    removeItem(id);
+  }
+}
 </script>
 
 <template>
-  <li class="flex hover:opacity-80">
+  <li
+    :class="classObject"
+    @pointerenter="isHover = true"
+    @pointerleave="isHover = false"
+  >
     <button
       class="overflow-hidden text-ellipsis whitespace-nowrap text-left w-full py-2"
+      @click="selectedItemId = id"
     >
       {{ title }}
     </button>
-    <button class="p-2">
-      <TrashIcon class="w-[16px]" />
-    </button>
+    <template v-if="isHover">
+      <button class="p-2" @click="deleteItem(id)">
+        <TrashIcon class="w-[16px]" />
+      </button>
+    </template>
   </li>
 </template>
